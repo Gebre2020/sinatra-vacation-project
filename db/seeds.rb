54bd82a362157305712a5d07883b8fc958970f97
Gebremeskel <gebre20122@gmail.com>
2021-03-04 08:30:11 -0600

    inserting data to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Packages.create(package_name: "A TRIP ABROAD", budget: 3000)
+Packages.create(package_name: "A TRIP TO THE BEACH", budget: 1500)
+Packages.create(package_name: "A CAMPING TRIP", budget: 1000)
+Packages.create(package_name: "A TRIP TO THE CITY", budget: 900)
+Packages.create(package_name: "A ROAD TRIP", budget: 700)
+Packages.create(package_name: "A CRUISE", budget: 2500)
+Packages.create(package_name: "A GROUP TOUR", budget: 800)
+Packages.create(package_name: "A GIRLS/GUYS TRIP", budget: 500)
+Packages.create(package_name: "A SOLO TRIP", budget: 600)
+
